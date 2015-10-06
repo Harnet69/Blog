182bfeb50cf2580b99de81f5a2297cec5e4ea70c
Harnet69 <motobycom@tut.by>
2015-10-06 08:42:11 +0300

    morning!

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+<title>Моя Тема WordPress для блога</title>
+</head>
+<body>
+	<div class="head-wrapper">
+		<div class="head">
+			<div class="head-logo"><img src="images/logo.jpg" alt="" /></div>
+			<div class="head-banner"><img src="images/728x90.jpg" alt="" /></div>
+		</div>
+	</div>
+	<div class="menu-wrapper">
+		<div class="menu-main">
+			<ul class="menu">
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About Me</a></li>
+				<li><a href="#">Design Services</a></li>
+				<li><a href="#">Request Quote</a></li>
+				<li><a href="#">Advertise</a></li>
+				<li><a href="#">Contact Me</a></li>
+			</ul>
+			<ul class="ico-social">
+				<li><a href="#"><img src="images/ico-vk.png" alt=""></a></li>
+				<li><a href="#"><img src="images/ico-yt.png" alt=""/></a></li>
+				<li><a href="#"><img src="images/ico-f.png" alt=""/></a></li>
+				<li><a href="#"><img src="images/ico-tw.png" alt=""/></a></li>
+			</ul>
+		</div>
+	</div>
+</body>
+</html>
